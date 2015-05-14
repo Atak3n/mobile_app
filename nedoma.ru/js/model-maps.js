b9a9445222e0bff26097a72b79d1ded05ed12e46
Рустем Кулыев <r.kouliev@nedoma.ru>
2015-05-14 04:56:23 +0000

    Добавлен автокомплит Добавлен глобальный объект с настройками карт

--- nedoma.ru/js/model-maps.js
+++ nedoma.ru/js/model-maps.js
@@ -1,3 +1,16 @@
+var MapGlobalObject={
+    autocomplete_data:["a","as","asd","asdf","asdfg","asdfgh","asdfghj"],
+    mapInit: function(){
+    	$('#map_canvas').height($('#map_role').height()-$('#map_role .km-navbar').height());
+    	//$('#map_canvas').height("300px");
+    	var map = L.map('map_canvas').setView([54.733333, 55.966667], 12);
+    	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+    	var markers = new L.FeatureGroup();
+    	markers.addLayer(L.marker([54.7386, 55.9633],{id: 1}).addTo(map).on('click',function(e){markers.clearLayers();}));
+    	markers.addLayer(L.marker([54.7486, 55.9633],{id: 2}).addTo(map).on('click',function(e){markers.clearLayers();}));
+    	map.addLayer(markers);
+	}
+};
 var mapModel = kendo.observable({
    	map_title:"Карта",
     isSearchVisible: false,
@@ -35,13 +48,9 @@ var mapModel = kendo.observable({
     }
 });
 
-var mapInit = function(){
-    $('#map_canvas').height($('#map_role').height()-$('#map_role .km-navbar').height());
-    //$('#map_canvas').height("300px");
-    var map = L.map('map_canvas').setView([54.733333, 55.966667], 12);
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
-    var markers = new L.FeatureGroup();
-    markers.addLayer(L.marker([54.7386, 55.9633],{id: 1}).addTo(map).on('click',function(e){markers.clearLayers();}));
-    markers.addLayer(L.marker([54.7486, 55.9633],{id: 2}).addTo(map).on('click',function(e){markers.clearLayers();}));
-    map.addLayer(markers);
-};
+$("#search_map").kendoAutoComplete({
+    dataSource: MapGlobalObject.autocomplete_data,
+    filter: "startswith",
+    placeholder: "Поиск...",
+    separator: ", "
+});
